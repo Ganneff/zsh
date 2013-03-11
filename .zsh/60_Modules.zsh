c478d244547cdfb51e3c5635769b850c71cabf1a
Joerg Jaspert <joerg@debian.org>
2013-03-11 01:11:18 +0100

    use 60_Modules for more module loading

--- .zsh/60_Modules.zsh
+++ .zsh/60_Modules.zsh
@@ -11,3 +11,17 @@ is4 && zmodload -i ${ZSHMODPREFIX}parameter
 # Gives strftime and $EPOCHSECONDS
 is4 && zmodload -i  ${ZSHMODPREFIX}datetime
 
+
+# Funky run-help hooks
+zrcautoload run-help-git
+zrcautoload run-help-svn
+
+# escape URLs automagically
+zrcautoload -U url-quote-magic
+zle -N self-insert url-quote-magic
+
+## Allow known mime types to be used as 'command'
+if is42; then
+    zrcautoload -U zsh-mime-setup
+    zsh-mime-setup
+fi
