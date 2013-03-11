e6ba173d5e771a875f0afe05cd4d1aae67dd24a6
Joerg Jaspert <joerg@debian.org>
2013-03-11 01:00:07 +0100

    New helper for colors

--- .zsh/05_Functions.zsh
+++ .zsh/05_Functions.zsh
@@ -69,3 +69,24 @@ if is434; then
         fi
     }
 fi
+
+docolors()
+{
+    if zrcautoload colors && colors 2>/dev/null ; then
+        for color in RED GREEN YELLOW BLUE MAGENTA CYAN WHITE; do
+            eval BOLD_$color='%{$terminfo[bold]$fg[${(L)color}]%}'
+            eval $color='%{$fg[${(L)color}]%}'
+            (( count = $count + 1 ))
+        done
+        NO_COLOR="%{${reset_color}%}"
+    else
+        BLUE=$'%{\e[1;34m%}'
+        RED=$'%{\e[1;31m%}'
+        GREEN=$'%{\e[1;32m%}'
+        CYAN=$'%{\e[1;36m%}'
+        WHITE=$'%{\e[1;37m%}'
+        MAGENTA=$'%{\e[1;35m%}'
+        YELLOW=$'%{\e[1;33m%}'
+        NO_COLOR=$'%{\e[0m%}'
+    fi
+}
