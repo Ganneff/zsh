cac120430f4701591dfd2eff1f24f0a40b7bb465
Joerg Jaspert <joerg@ganneff.de>
2013-03-22 13:34:00 +0100

    Better debug output

--- .zsh/90_Plugins.zsh
+++ .zsh/90_Plugins.zsh
@@ -7,10 +7,11 @@
 HISTORY_BASE=${ZDOTDIR}/var/dirhist
 mkdir -p ${HISTORY_BASE}
 
+debug ""
 if [ -d ${ZDOTDIR}/plugins ]; then
     for file in ${ZDOTDIR}/plugins/*.zsh; do
-        debug "Loading ${file}"... -n
+        debug "PLUGIN: Loading ${file}"... -n
         source $file
-        debug "$fg_no_bold[green]done"
+        debug "PLUGIN: $fg_no_bold[green]done"
     done
 fi
