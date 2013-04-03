7c9a07ed61e31b5677b9e425ed382e4780dd2a27
Joerg Jaspert <joerg@ganneff.de>
2013-04-03 10:16:02 +0200

    also ignore error level 8 for screen

--- .zsh/95_tmuxscreen.zsh
+++ .zsh/95_tmuxscreen.zsh
@@ -12,7 +12,12 @@ if zstyle -T ':ganneff:config' termstatus; then
 
     if is-callable screen; then
         screen -q -ls
-        if [[ $? -ne 9 ]]; then
+        # According to the manpage the exit level with -q can be one
+        # of 9==no sessions, 10==not attachable sessions,
+        # 11 (or more)==sessions.
+        # But of course there are bugs, and so 8 is also returned in
+        # certain conditions when there are no sessions.
+        if [[ $? -ne 9 ]] || [[ $? -ne 8 ]]; then
             preprint "screen sessions" && echo
             screen -ls
             preprint && echo
